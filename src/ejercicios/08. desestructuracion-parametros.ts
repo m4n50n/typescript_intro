7b9947af77ff48a249ba341e1e7d778da285da64
Jose Clemente Garcia Rodriguez <jose.garcia@digitalvirgo.com>
2022-05-22 11:47:25 +0200

    desestructuracion de parametros

---
+++ src/ejercicios/08. desestructuracion-parametros.ts
@@ -0,0 +1,34 @@
+/*
+    ===== Código de TypeScript =====
+*/
+
+interface Producto {
+    desc: string;
+    precio: number;
+}
+
+const telefono: Producto = {
+    desc: "Nokia A1",
+    precio: 20
+};
+
+const tablet: Producto = {
+    desc: "Apple 1",
+    precio: 500
+};
+
+const calcular = (productos: Producto[]): [number, number] => {
+    let total = 0;
+
+    productos.forEach(({ precio }) => { // Poniendo precio entre llaves desestructuramos los parámetros
+        total += precio;
+    });
+
+    return [total, total * 0.15];
+}
+
+const articulos = [telefono, tablet];
+const [total, totaloperacion] = calcular(articulos); // Desestructuración del array devuelto
+
+console.log("total", total);
+console.log("totaloperacion", totaloperacion);
--- src/index.ts
+++ src/index.ts
@@ -2,32 +2,4 @@
     ===== Código de TypeScript =====
 */
 
-interface Producto {
-    desc: string;
-    precio: number;
-}
 
-const telefono: Producto = {
-    desc: "Nokia A1",
-    precio: 20.3
-};
-
-const tablet: Producto = {
-    desc: "Apple 1",
-    precio: 500.6
-};
-
-function calcular(productos: Producto[]) {
-    let total = 0;
-
-    productos.forEach((producto: Producto) => {
-        total += producto.precio;
-    });
-
-    return total * 0.15;
-}
-
-const articulos = [telefono, tablet];
-const operacion = calcular(articulos);
-
-console.log("Resultado", operacion);
